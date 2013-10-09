4580e5489ca8b29066d405c7f3c1a71f6a1029d1
Bertrand Dunogier <bd@ez.no>
2013-10-09 23:18:59 +0200

    Revert "EZP-21213: forgotten stashed changes from last commit"
    This reverts commit 60a8556706dfa75f3bafbfafa0f18b6323a8933d.

--- eZ/Publish/SPI/Tests/FieldType/ImageIntegrationTest.php
+++ eZ/Publish/SPI/Tests/FieldType/ImageIntegrationTest.php
@@ -57,7 +57,7 @@ class ImageIntegrationTest extends FileBaseIntegrationTest
      */
     protected function getStoragePrefix()
     {
-        return '';
+        return 'images';
     }
 
     /**
